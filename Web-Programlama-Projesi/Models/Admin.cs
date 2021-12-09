ef1723b7fe50700010cb3c8c6150f4c32239e9f9
MertcanDemiralan <62715500+MertcanDemiralan@users.noreply.github.com>
2021-12-10 00:23:49 +0300

    Add files via upload

---
+++ Web-Programlama-Projesi/Models/Admin.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web_Programlama_Projesi.Models
+{
+    public class Admin:Kisi
+    {
+
+    }
+}

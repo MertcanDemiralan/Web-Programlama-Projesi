88145500233be9d18a407bfa486a1fb643426cdf
Gokay Iseri <62775025+SkyMoonI@users.noreply.github.com>
2021-12-15 22:41:58 +0300

    Add files via upload

---
+++ Migrations/20211215120835_Kategori.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebProje.Migrations
+{
+    public partial class Kategori : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Kategori_Adı",
+                table: "Kategoriler",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Kategori_Adı",
+                table: "Kategoriler",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
